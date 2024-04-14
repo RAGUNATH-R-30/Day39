bc75cc1c4fe307b0c88a82611b49113780959183
RAGUNATH-R-30 <ragunath3003@gmail.com>
2024-04-14 20:20:51 +0530

    dayend_

--- script.js
+++ script.js
@@ -98,21 +98,44 @@ app.put("/assignmultiplestudents",async(req,res)=>{
   try {
     const connection = await MongoClient.connect(URL)
     const db = connection.db("node_mongo");
-    const student_collection = db.collection("students")
-    const mentor_collection = db.collection("mentors")
+    const collection = db.collection("students")
+    const mentorcollection = db.collection("mentors")
     let mentor_id = new ObjectId(req.body.mentor_id)
-    let students = req.body.students
 
+    const mentor=await mentorcollection.findOne({_id:mentor_id})
+    let studentIds = req.body.students.map((studentId)=>new ObjectId(studentId))
+    
+    for (const studentId of studentIds){
 
+      const student = await collection.findOne({_id:studentId})
+      const previous_mentor = student.mentor
+      await collection.updateOne({_id:studentId},{$set:{
+        previous_mentors:previous_mentor,
+        mentor:{"_id":mentor._id,"name":mentor.name,"age":mentor.age,"Expertise":mentor.expertise}}}
+      )
+      await mentorcollection.updateOne({_id:mentor_id},{$push:{students:student}})
+    }
+    await connection.close();
+    res.json("success");
 
-    await mentor_collection.updateOne({_id:mentor_id},{
-      $push:{
-        students:{$each:students}
-      }
+    // students.map(async(studentid)=>{
+    //   let student_id = new ObjectId(studentid)
+   
+    //   const mentor=await mentorcollection.findOne({_id:mentor_id})
+    //   const student = await collection.findOne({_id:student_id})
+    //   const previous_mentor = student.mentor
+    //   await collection.updateOne({_id:student_id},{$set:{previous_mentors:previous_mentor.name}})
+    //   await collection.updateOne({_id:student_id},{$set:{mentor:{"_id":mentor._id,"name":mentor.name,"age":mentor.age,"Expertise":mentor.expertise}}})
+    //   await mentorcollection.updateOne({_id:mentor_id},{$push:{students:student}})
+    // })
+    // await mentor_collection.updateOne({_id:mentor_id},{
+    //   $push:{
+    //     students:{$each:students}
+    //   }
       // $set:{students:students}
-    })
-    await connection.close()
-    res.json("success")
+    // })
+    // await connection.close()
+    // res.json("success")
   } catch (error) {
     
     console.log(error);
@@ -130,9 +153,9 @@ app.get("/getmentorstudents",async(req,res)=>{
     const mentor_collection = db.collection("mentors")
     const student_collection = db.collection("students")
     const mentor = await mentor_collection.findOne({_id:mentor_id})
-
-    console.log(mentor.name)
+    console.log()
     res.json({"mentorname":mentor.name,"students":mentor.students})
+    await connection.close()
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Something Went Wrong" });   
@@ -150,8 +173,30 @@ app.get("/previousmentors",async(req,res)=>{
     const student = await student_collection.findOne({_id:student_id})
     console.log(student)
     res.json(student.previous_mentors)
-
+    await connection.close()
   } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something Went Wrong" });   
+  }
+})
+
+app.get("/nomentors",async(req,res)=>{
+  try {
+    const connection = await MongoClient.connect(URL)
+    const db = connection.db("node_mongo")
+    const student_collection = db.collection("students")
+    let nomentors = []
+    let allstudents = await student_collection.find().toArray()
+    allstudents.map((student)=>{
+      student.mentor == ""&& nomentors.push(student)
+    })
+    console.log(allstudents)
+    await connection.close()
+    res.json({"message":"sucess","Nomentors":nomentors})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something Went Wrong" });   
     
   }
+
 })
